036f5aaf4c8853fbb22ebf001ab992834eacc613
Bruno Albuquerque <bga@gmail.com>
2023-11-07 21:33:50 +0000

    Fix AccessType enum.

--- unity/key/access_type.go
+++ unity/key/access_type.go
@@ -1,10 +1,11 @@
 package key
 
+// AccessType is the type of access allowed for a specific key.
 type AccessType int32
 
 const (
 	AccessTypeNone = iota
-	AccessTypeRead
+	AccessTypeRead = 1 << (iota - 1)
 	AccessTypeWrite
 	AccessTypeAction
 )
