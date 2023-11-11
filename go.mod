921399eb353c5536337c284bfcd4c88e3e3bc480
Bruno Albuquerque <bga@gmail.com>
2023-11-11 21:12:44 +0000

    Logger improvements.
    - Remove SetLevel/Reset as it would play havoc with multithreading.
    - Added TTY detection and color support on Windows.

--- go.mod
+++ go.mod
@@ -4,4 +4,12 @@ go 1.21.1
 
 require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 
-require github.com/lmittmann/tint v1.0.3
+require (
+	github.com/lmittmann/tint v1.0.3
+	github.com/mattn/go-isatty v0.0.20
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13
+	golang.org/x/sys v0.6.0 // indirect
+)
--- support/logger/logger.go
+++ support/logger/logger.go
@@ -3,50 +3,32 @@ package logger
 import (
 	"log/slog"
 	"os"
-	"sync"
 
 	"github.com/lmittmann/tint"
+	"github.com/mattn/go-colorable"
+	"github.com/mattn/go-isatty"
 )
 
 type Logger struct {
 	*slog.Logger
 
-	m             sync.Mutex
-	previousLevel slog.Level
-	levelVar      *slog.LevelVar
+	levelVar *slog.LevelVar
 }
 
 func New(level slog.Level) *Logger {
 	levelVar := &slog.LevelVar{}
 	levelVar.Set(level)
 
+	output := os.Stdout
+
 	opts := &tint.Options{
-		Level: levelVar,
+		Level:   levelVar,
+		NoColor: !isatty.IsTerminal(output.Fd()),
 	}
 
 	return &Logger{
-		Logger:        slog.New(tint.NewHandler(os.Stdout, opts)),
-		previousLevel: level,
-		levelVar:      levelVar,
+		Logger: slog.New(tint.NewHandler(colorable.NewColorable(output),
+			opts)),
+		levelVar: levelVar,
 	}
-}
-
-func (l *Logger) SetLevel(level slog.Level) {
-	l.m.Lock()
-
-	l.previousLevel = l.levelVar.Level()
-	l.levelVar.Set(level)
-
-	l.m.Unlock()
-}
-
-func (l *Logger) ResetLevel() {
-	l.m.Lock()
-
-	currentLevel := l.levelVar.Level()
-
-	l.levelVar.Set(l.previousLevel)
-	l.previousLevel = currentLevel
-
-	l.m.Unlock()
 }
