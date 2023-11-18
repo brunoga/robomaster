6442dadb285f3e178e4b0f351eb29fd720749623
Bruno Albuquerque <bga@gmail.com>
2023-11-18 12:36:05 -0500

    Improve Result type.

--- unity/result/result.go
+++ unity/result/result.go
@@ -28,6 +28,17 @@ type jsonResult struct {
 	Value any
 }
 
+func New(key *key.Key, tag uint64, errorCode int32, errorDesc string,
+	value any) *Result {
+	return &Result{
+		key:       key,
+		tag:       tag,
+		errorCode: errorCode,
+		errorDesc: errorDesc,
+		value:     value,
+	}
+}
+
 // NewFromJSON creates a new Result from the given JSON data. Any errors are
 // reported in the Result itself and should be handled by anyone that cares
 // about it.
@@ -52,13 +63,14 @@ func NewFromJSON(jsonData []byte) *Result {
 
 	key, err := key.FromSubType(jr.Key)
 	if err != nil {
+		fmt.Printf("error creating key from sub type %d: %s\n", jr.Key, err.Error())
 		r.errorCode = -1
 		r.errorDesc = fmt.Sprintf("error creating key from sub type %d: %s",
 			jr.Key, err.Error())
 		return r
 	}
 
-	errorDesc := "no error"
+	errorDesc := ""
 	if jr.Error != 0 {
 		errorDesc = fmt.Sprintf("error %d", jr.Error)
 	}
@@ -93,6 +105,11 @@ func (r *Result) Key() *key.Key {
 	return r.key
 }
 
+// SetKey sets the key associated with this result.
+func (r *Result) SetKey(key *key.Key) {
+	r.key = key
+}
+
 // Tag returns the tag associated with this result.
 func (r *Result) Tag() uint64 {
 	return r.tag
@@ -103,14 +120,29 @@ func (r *Result) ErrorCode() int32 {
 	return r.errorCode
 }
 
+// SetErrorCode sets the error code associated with this result.
+func (r *Result) SetErrorCode(errorCode int32) {
+	r.errorCode = errorCode
+}
+
 // ErrorDesc returns the error description associated with this result.
 func (r *Result) ErrorDesc() string {
 	return r.errorDesc
 }
 
+// SetErrorDesc sets the error description associated with this result.
+func (r *Result) SetErrorDesc(errorDesc string) {
+	r.errorDesc = errorDesc
+}
+
 // Value returns the value associated with this result.
 func (r *Result) Value() any {
 	return r.value
+}
+
+// SetValue sets the value associated with this result.
+func (r *Result) SetValue(value any) {
+	r.value = value
 }
 
 // Succeeded returns true if this result represents a successful operation.
@@ -122,4 +154,15 @@ func (r *Result) Succeeded() bool {
 func (r *Result) String() string {
 	return fmt.Sprintf("Result{Key: %s, Tag: %d, ErrorCode: %d, ErrorDesc: "+
 		"%s, Value: %v}", r.key, r.tag, r.errorCode, r.errorDesc, r.value)
+}
+
+func (r *Result) MarshalJSON() ([]byte, error) {
+	jr := jsonResult{
+		Key:   r.key.SubType(),
+		Tag:   r.tag,
+		Error: r.errorCode,
+		Value: jsonResultValue{r.value},
+	}
+
+	return json.Marshal(jr)
 }
