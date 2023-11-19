2fd62eb3f5419f68012d4b8e19b3ae422cb04a72
Bruno Albuquerque <bga@gmail.com>
2023-11-19 16:52:55 -0500

    Clean up controller component.

--- sdk2/examples/robotcontrol/components/controller.go
+++ sdk2/examples/robotcontrol/components/controller.go
@@ -6,15 +6,10 @@ import (
 
 type Controller struct {
 	Controller *controller.Controller
-
-	PreviousLeftRight       float32
-	PreviousForwardBackward float32
 }
 
 func NewController(r *Robomaster) (*Controller, error) {
 	return &Controller{
 		r.Client().Controller(),
-		0.0,
-		0.0,
 	}, nil
 }
