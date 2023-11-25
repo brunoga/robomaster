0082de6bfe2a18c6a4531dd139b5421ce2d26ef5
Bruno Albuquerque <bga@gmail.com>
2023-11-25 13:38:03 -0500

    Firing now works.
    - Support both beads and infrared.
    - Implement Connected() and WaitForConnection in a more sensible way.

--- sdk2/module/gun/gun.go
+++ sdk2/module/gun/gun.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/brunoga/robomaster/sdk2/module"
+	"github.com/brunoga/robomaster/sdk2/module/robot"
 	"github.com/brunoga/unitybridge"
 	"github.com/brunoga/unitybridge/support/logger"
 	"github.com/brunoga/unitybridge/unity/key"
@@ -15,12 +16,14 @@ import (
 type Gun struct {
 	ub unitybridge.UnityBridge
 	l  *logger.Logger
+	r  *robot.Robot
 }
 
 var _ module.Module = (*Gun)(nil)
 
 // New creates a new Gun instance.
-func New(ub unitybridge.UnityBridge, l *logger.Logger) (*Gun, error) {
+func New(ub unitybridge.UnityBridge, l *logger.Logger,
+	r *robot.Robot) (*Gun, error) {
 	if l == nil {
 		l = logger.New(slog.LevelError)
 	}
@@ -30,51 +33,67 @@ func New(ub unitybridge.UnityBridge, l *logger.Logger) (*Gun, error) {
 	return &Gun{
 		ub: ub,
 		l:  l,
+		r:  r,
 	}, nil
 }
 
 // Start starts the Gun module.
 func (g *Gun) Start() error {
-	return nil
+	return g.r.EnableFunction(robot.FunctionTypeGunControl, true)
 }
 
-// Connected returns whether the Gun module is connected. It always returns
-// true as there is no connection to handle.
+// Connected returns whether the Gun module is connected.
 func (g *Gun) Connected() bool {
-	return true
+	return g.r.HasDevice(robot.DeviceTypeWaterGun)
 }
 
-// WaitForConnection waits for the Gun module to connect. It always returns
-// immediately as there is no connection to handle.
+// WaitForConnection waits for the Gun module to connect and returns the
+// connected status. The timeout parameter is ignored as it always returns
+// immediately.
 func (g *Gun) WaitForConnection(timeout time.Duration) bool {
-	return true
+	return g.r.HasDevice(robot.DeviceTypeWaterGun)
 }
 
-// Fire fires the gun.
-//
-// TODO(bga): The id parameter seems to always be 0 or 1.
-func (g *Gun) Fire(typ Type, id uint8) error {
-	if !typ.Valid() {
-		return fmt.Errorf("invalid gun type %d", typ)
-	}
-
-	var k *key.Key
+// Fire fires the Gun module with the given type.
+func (g *Gun) Fire(typ Type) error {
 	switch typ {
 	case TypeBead:
-		k = key.KeyRobomasterWaterGunWaterGunFire
+		// TODO(bga): Maybe implement firing multiple beads and extend it to IR
+		//            firing too.
+		return g.fireBead(1)
 	case TypeInfrared:
-		k = key.KeyRobomasterInfraredGunInfraredGunFire
+		return g.fireInfrared()
 	}
 
-	return g.ub.DirectSendKeyValue(k, uint64(id))
+	return fmt.Errorf("invalid gun type: %v", typ)
 }
 
 // Stop stops the Gun module.
 func (g *Gun) Stop() error {
-	return nil
+	return g.r.EnableFunction(robot.FunctionTypeGunControl, false)
 }
 
 // String returns a string representation of the Gun module.
 func (G *Gun) String() string {
 	return "Gun"
+}
+
+type timesValue struct {
+	Value uint64 `json:"value"`
+}
+
+func (g *Gun) fireBead(times uint64) error {
+	return g.ub.PerformActionForKey(key.KeyRobomasterWaterGunWaterGunFireWithTimes,
+		timesValue{times}, nil)
+}
+
+func (g *Gun) fireInfrared() error {
+	go func() {
+		// Disable firing after a while.
+		time.Sleep(200 * time.Millisecond)
+
+		g.ub.DirectSendKeyValue(key.KeyRobomasterWaterGunWaterGunFire, uint64(0))
+	}()
+
+	return g.ub.DirectSendKeyValue(key.KeyRobomasterWaterGunWaterGunFire, uint64(1))
 }
