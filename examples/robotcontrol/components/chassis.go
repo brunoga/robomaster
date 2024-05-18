5b4f26f9e4b2a407e94272dbd1fe487326e86c73
Bruno Albuquerque <bga@gmail.com>
2024-05-18 08:23:21 -0400

    Move robotcontrol to its own repo.
    - https://github.com/brunoga/robomaster-control

--- examples/robotcontrol/components/chassis.go
+++
@@ -1,7 +0,0 @@
-package components
-
-import "github.com/brunoga/robomaster/module/chassis"
-
-type Chassis struct {
-	Chassis *chassis.Chassis
-}
--- examples/robotcontrol/components/gun.go
+++
@@ -1,7 +0,0 @@
-package components
-
-import "github.com/brunoga/robomaster/module/gun"
-
-type Gun struct {
-	Gun *gun.Gun
-}
