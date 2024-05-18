7a59755ba4fbdb976fc31ee90408c09b8eb0092b
Bruno Albuquerque <bga@gmail.com>
2024-05-18 07:55:17 -0400

    Remove old sdk dir.
    - Not even DJI maintains this method anymore.

--- sdk/support/README.md
+++
@@ -1,3 +0,0 @@
-This directory contains code not directly related to the RoboMaster SDK but that
-can be used to simplify the creation of interesting/useful programs in an easier
-way.
--- sdk/support/pid/controller.go
+++
@@ -1,8 +0,0 @@
-package pid
-
-// Controller is the interface for generic controllers that generate an output
-// based on some input parameter.
-type Controller interface {
-	// Output returns an output associated with the given input.
-	Output(float64) float64
-}
