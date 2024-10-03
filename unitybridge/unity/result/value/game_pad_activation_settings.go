5c556dcbcb1995863fd3e60a18ca57b8258220c7
Bruno Albuquerque <bga@gmail.com>
2024-10-03 10:45:08 -0400

    Fix member names.

--- unitybridge/unity/result/value/game_pad_activation_settings.go
+++ unitybridge/unity/result/value/game_pad_activation_settings.go
@@ -1,7 +1,7 @@
 package value
 
 type GamePadActivationSettings struct {
-	IsActivated  bool   `json:"isActivated"`
-	ActivateTime int64  `json:"activateTime"`
-	SerialNumber string `json:"serialNumber"`
+	IsActivated  bool   `json:"IsActivated"`
+	ActivateTime int64  `json:"ActivateTime"`
+	SerialNumber string `json:"SerialNumber"`
 }
